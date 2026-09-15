e283e7486c9ae217aef2b262331a843847aa3eb0
Pablo Oliva <pabloadra1@gmail.com>
2026-09-15 22:16:00 +0200

    More exercise

--- practica-rars/basic_L2/1-exercise_1.s
+++ practica-rars/basic_L2/1-exercise_1.s
@@ -4,14 +4,17 @@
 #-- ¿Cuantos bytes de memoria ocupa el programa?
 
 .text
-
-li x5, 1
+#-- valores inciales
+li x5, 0
 li x6, 1
 
-add x8, x5, x6
-add x9, x6, x8
-add x10, x8, x9
-add x11, x9, x10
+bucle:
+#-- calcular siguiente término en t2
+add x7, x5, x6                      #-- x7 = x5+x6 = 0+1 = 1
+#-- actualizas registros
+add x5, x0, x6                      #-- x5 pasa a x6 -> 0 = 1
+add x6, x0, x7                      #-- x6 pasa a x7 -> 1 = 1
+j bucle
 
-ecall
+
 
---
+++ practica-rars/basic_L2/2-exercise_2.s
@@ -0,0 +1,13 @@
+#-- Exercise 4
+#-- Write an assambly program to calculate the expression: 
+#-- "f = (a + b + c) - [(d - a) + (e + 3) - 30]"
+#-- Implement the expression as is, without applying simplifications or prior calculations.
+#-- Check the result for variable values: a=2, B=4, c=6, d=8
+
+.text
+
+li x5, 2            #-- a=2 
+li x6, 4            #-- b=4
+li x7, 6            #-- c=6
+li x28, 8           #-- d=8 
+
