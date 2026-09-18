be99adc2ec1df163157593b0493fa64b0a8dddb5
Pablo Oliva <pabloadra1@gmail.com>
2026-09-18 23:53:30 +0200

    updated exercise

--- practica-rars/basic_L2/exercise_6.s
+++ practica-rars/basic_L2/exercise_6.s
@@ -2,7 +2,16 @@
 
 #-- Pasamos a ver los datos (data).
 #-- Vamos a almacenar palabras en el segmento de datos.
+#-- Siempre que vamos a almacenar palabras dentro de data, hay que definirlas como ".word"
 
 .data
 
-g
+a: .word 0xBEBECAFE, 1, 2
+b: .word 0xFACEB00C, 0XABCDEF0
+
+.text
+
+
+#-- finalizar programa
+li a7, 10
+ecall
